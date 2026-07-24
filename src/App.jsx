98c402d6c3b21a258ccbb1da52628b8bf9c1959d
Protiksha Khanikar <pratikshakhanikar@gmail.com>
2026-07-24 23:59:31 +0530

    current progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
-export const App = () => {
+import Navbar from './components/Navbar'
+const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Navbar />
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <div>
+            {/* {logo} */}
+            <div>
+                <span></span>
+                <span>Scooby</span>
+            </div>
+
+            {/* {Desktop menu} */}
+            <nav>
+                <a href="/">Home</a>
+                <div></div>
+                <a href="/">About"</a>
+                <a href="/">Shop"</a>
+                <a href="/">Blog"</a>
+                <a href="/">Contact"</a>
+            </nav>
+
+            {/* {icons} */}
+         
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
